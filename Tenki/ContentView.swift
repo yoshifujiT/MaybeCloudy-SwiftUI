d2aacb6d74ca82ff45ead2075dc5949f753e5ef5
yoshifuji <t.yoshifuji@me.com>
2020-07-04 21:55:00 +0900

    Init

---
+++ Tenki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tenki
+//
+//  Created by ToruYoshifuji on 2020/07/04.
+//  Copyright © 2020 ToruYoshifuji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
