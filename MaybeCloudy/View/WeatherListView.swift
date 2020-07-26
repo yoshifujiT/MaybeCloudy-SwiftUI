534e70e23126a438d3a3874437c62cfecc59dd37
yoshifuji <t.yoshifuji@me.com>
2020-07-26 21:29:50 +0900

    Fix list view styles

--- MaybeCloudy/View/WeatherListView.swift
+++ MaybeCloudy/View/WeatherListView.swift
@@ -10,54 +10,77 @@ import SwiftUI
 
 struct WeatherListView: View {
     @ObservedObject private var weatherListVM = WeatherListViewModel()
+    let dt = Date()
+    let dateFormatter = DateFormatter()
     
     func getCityItem(cityId: Int) -> CityData {
         let cityItem = cityData.first(where: { $0.id == cityId})!
         return cityItem
     }
+    
+    func getDate() -> String {
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "Md", options: 0, locale: Locale(identifier: "ja_JP"))
+        return dateFormatter.string(from: dt)
+    }
 
     var body: some View {
-        ZStack {
-            Color("Gray01")
-                .edgesIgnoringSafeArea(.all)
-    
-            VStack(spacing: 0) {
-                AppBar()
-                NavigationView {
-                    GeometryReader { geometry in
-                        ScrollView(showsIndicators: false){
-                            VStack(spacing: 0) {
-                                ForEach(self.weatherListVM.weatherData) { weather in
-                                    // TODO: ここでローカル変数cityを宣言してdestinationとRowで使いたい
-                                    NavigationLink(
-                                        destination: WeatherDetailView(
-                                            weather: weather,
-                                            city: self.getCityItem(cityId: weather.id)
-                                        )
-                                    ) {
-                                        WeatherListRow(
-                                            weather: weather,
-                                            city: self.getCityItem(cityId: weather.id)
-                                        )
+        VStack(spacing: 0) {
+            AppBar()
+            NavigationView {
+                ZStack {
+                    Color("Gray01")
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    VStack {
+                        GeometryReader { geometry in
+                            ScrollView(showsIndicators: false){
+                                Spacer()
+                                    .frame(height: 16)
+                                VStack(alignment: .leading, spacing: 0) {
+                                    Text("お気に入り都道府県の \(self.getDate()) の天気")
+                                        .foregroundColor(Color.black)
+                                        .fontWeight(.bold)
+                                        .padding(.top, 8)
+                                        .padding(.bottom, 12)
+                                    VStack(spacing: 0) {
+                                        ForEach(self.weatherListVM.weatherData) { weather in
+                                            // TODO: ここでローカル変数cityを宣言してdestinationとRowで使いたい
+                                            NavigationLink(
+                                                destination: WeatherDetailView(
+                                                    weather: weather,
+                                                    city: self.getCityItem(cityId: weather.id)
+                                                )
+                                            ) {
+                                                WeatherListRow(
+                                                    weather: weather,
+                                                    city: self.getCityItem(cityId: weather.id)
+                                                )
+                                            }
+                                            if weather.id != self.weatherListVM.weatherData.last!.id {
+                                                Rectangle()
+                                                    .fill(Color("Gray02"))
+                                                    .frame(height: 1)
+                                            }
+                                        }
                                     }
-                                    if weather.id != self.weatherListVM.weatherData.last!.id {
-                                        Rectangle()
-                                            .fill(Color("Gray02"))
-                                            .frame(height: 1)
-                                    }
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 16)
+                                    .background(Color.white)
+                                    .frame(width: geometry.size.width)
                                 }
+                                Spacer()
+                                    .frame(height: 16)
                             }
-                            .padding(.horizontal, 16)
-                            .frame(width: geometry.size.width)
+                            .navigationBarTitle("")
+                            .navigationBarHidden(true)
                         }
-                        .navigationBarTitle("")
-                        .navigationBarHidden(true)
                     }
+                    .padding(.horizontal, 16)
+                    .shadow(color: Color.black.opacity(0.05), radius: 4, x: 0, y: 2)
                 }
-                .shadow(color: Color.black.opacity(0.05), radius: 4, x: 0, y: 2)
-                .padding(16)
-            }.edgesIgnoringSafeArea(.top)
+            }
         }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
